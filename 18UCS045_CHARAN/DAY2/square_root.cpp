75b30e02460b76c602a0b5b092b0e9a1b80555c8
charanchows <74544163+charanchows@users.noreply.github.com>
2021-05-24 22:35:58 +0530

    Update square_root.cpp

--- 18UCS045_CHARAN/DAY2/square_root.cpp
+++ 18UCS045_CHARAN/DAY2/square_root.cpp
@@ -1,1 +1,16 @@
-
+int Solution::sqrt(int A)
+{
+ if(A==0) return 0;
+int low=0, hi=A;
+while(low<=hi)
+{
+long long mid=low+(hi-low)/2;
+if(mid*mid==A)
+{
+    return mid;
+}
+if(mid*mid>A) hi=mid-1;
+else low=mid+1;
+}
+return low-1;
+}
